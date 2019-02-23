b5ca45f47f833440114fdcd5997a77f68d10c87e
Javier <jcorrochano98@gmail.com>
2019-02-23 13:06:04 +0100

    fotos y filtrado arreglado

--- microp2/src/app/app.component.ts
+++ microp2/src/app/app.component.ts
@@ -10,32 +10,26 @@ import { EstadoCoche } from './coche';
 export class AppComponent {
   title = 'MicroP2';
 
-  public EstadoCoche:any = EstadoCoche;  // ESTA LINEA SOLO SIRVE PARA PODER USAR EstadoCoche en el TEMPLATE.
-  public coches:Array<Coche>=[new Coche('renault','scenic',new Date(2007,10,1),new Date(2018,3,1),5000,'https://www.farosypilotos.es/wp-content/uploads/2017/04/grand_scenic-REAR.jpg',EstadoCoche.BUENO),
-  new Coche('renault','scenic',new Date(2007,10,1),new Date(2018,3,1),5000,'https://www.farosypilotos.es/wp-content/uploads/2017/04/grand_scenic-REAR.jpg',EstadoCoche.BUENO),new Coche('renault','scenic',
-  new Date(2007,10,1),new Date(2018,3,1),5000,'https://www.farosypilotos.es/wp-content/uploads/2017/04/grand_scenic-REAR.jpg',EstadoCoche.BUENO), new Coche('tesla','model 3',new Date(2007,10,1),new Date(2018,3,1),5000,'https://amp.businessinsider.com/images/5c6d714a262898671d5920d2-750-563.jpg',EstadoCoche.BUENO),
-  new Coche('fiat','panda',new Date(2007,10,1),new Date(2018,3,1),5000,'https://www.autobild.es/sites/autobild.es/public/styles/main_element/public/dc/fotos/Fiat-Panda_2013_C01.jpg?itok=LxJTvYeN',EstadoCoche.MALO)];
+  public EstadoCoche: any = EstadoCoche;  // ESTA LINEA SOLO SIRVE PARA PODER USAR EstadoCoche en el TEMPLATE.
+  public coches: Array<Coche> = [new Coche('renault', 'scenic', new Date(2007, 10, 1), new Date(2018, 3, 1), 5000, '../assets/scenic.jpg', EstadoCoche.BUENO),
+  new Coche('renault', 'megane', new Date(2007, 10, 1), new Date(2018, 3, 1), 5000, '../assets/megane.png', EstadoCoche.BUENO), new Coche('renault', 'clio',
+    new Date(2007, 10, 1), new Date(2018, 3, 1), 5000, '../assets/clio.jpg', EstadoCoche.BUENO), new Coche('tesla', 'model 3', new Date(2007, 10, 1), new Date(2018, 3, 1), 5000, '../assets/tesla.jpg', EstadoCoche.BUENO),
+  new Coche('fiat', 'panda', new Date(2007, 10, 1), new Date(2018, 3, 1), 5000, "", EstadoCoche.MALO)];
 
-  public getCoches():Array<Coche> {
+  public getCoches(): Array<Coche> {
     return this.coches;
   }
 
-  public filtro:string='';
+  public filtro: string = '';
 
-  public getFiltrados():Array<Coche> {
-    var counter:number = 0;
-    var lista:Array<Coche>=[];
-    if(this.filtro != '') {
-      for(let i of this.coches) {
-        if(i.marca.lastIndexOf(this.filtro, 0) === 0) {
-          lista.push(this.coches[counter]);
-        }
-        counter++;
-      }
-    } else {
-      lista = this.coches;
+  public getFiltrados(): Array<number> {
+    var filtrados: Array<number> = [];
+    var indice: number = 0;
+    for (var coche of this.coches) {
+      if (coche.marca.lastIndexOf(this.filtro, 0) === 0) filtrados.push(indice);
+      indice = indice + 1;
     }
-    return lista;
+    return filtrados;
   }
 
   public comp_rebajar(i) {
@@ -43,6 +37,6 @@ export class AppComponent {
   }
 
   public comp_vendido(i) {
-    this.coches.splice(i,1);
+    this.coches.splice(i, 1);
   }
 }
