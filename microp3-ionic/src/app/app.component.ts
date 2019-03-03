affef1ea7c40bc511d0e6c9657d0a3a8dcf7d3bb
Javier <jcorrochano98@gmail.com>
2019-03-03 11:58:25 +0100

    MICROP3 IONIC

---
+++ microp3-ionic/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { Pag1Page } from '../pages/pag1/pag1';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = Pag1Page;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+}
+
