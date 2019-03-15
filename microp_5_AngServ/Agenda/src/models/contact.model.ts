5fb845f9ababdf3e45c83f27831d34df9386aadf
Javier <jcorrochano98@gmail.com>
2019-03-15 22:31:54 +0100

    Micropractica 5 FUNCIONANDO

---
+++ microp_5_AngServ/Agenda/src/models/contact.model.ts
@@ -0,0 +1,9 @@
+export interface Contact {
+
+    key?: string; //Esto maneja el id de contacto al usar firebase
+    nombre: string;
+    organizacion: string;
+    movil: string;
+    correo: string;
+
+}
